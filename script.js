9a1590e97fc6b392e74cd93c45d01b0a62e261e0
nivethaofs <32072857+nivethaofs@users.noreply.github.com>
2017-10-25 21:47:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var SanTechApp = angular.module('SanTechApp', ["ui.router"]);
+
+// ui routing
+SanTechApp.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+  .state("/", {
+    url: "/home",
+    templateUrl: 'views/home.html'
+  })
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state("home", {
+      url: "/home",
+      templateUrl: 'views/home.html'
+    })
+
+    .state("flight", {
+      url: "/flight",
+      templateUrl: 'views/flight.html'
+    });
+});
